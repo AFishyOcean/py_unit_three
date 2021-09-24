2a29781fe48927ef33170eec653701145fa46f49
AFishyOcean <dljhazelh@gmail.com>
2021-09-24 09:00:36 -0400

    Random issues

--- addition.py
+++ addition.py
@@ -1,7 +1,7 @@
 # Write your addition function in the space below...
 def add_two(x, y):
     addition = x + y
-    print("The sum of", x, "plus", y, "is", addition)
+    print("The sum of", x, "and", y, "is", addition)
 
 # Do not change anything below these lines
 def main():
