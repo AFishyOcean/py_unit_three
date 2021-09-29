110fd44960aeb6ceb536dbc9b09055a1ab848a11
AFishyOcean <dljhazelh@gmail.com>
2021-09-29 11:29:13 -0400

    Did returning tests

---
+++ assignment_three.py
@@ -0,0 +1,18 @@
+print("This program calculates the surface area of a rectangular prism")
+def get_width(x):
+    x = input("What is the width")
+    return x
+def get_height(y):
+    y = input("What is the height")
+    return y
+def get_length(z):
+    z = input("What is the length")
+    return z
+
+def main():
+    get_height(y)
+    get_width(x)
+    get_length(z)
+
+
+main()
