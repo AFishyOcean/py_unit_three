cafd9283e14d78437a7c19542b5b7ecba75760f5
AFishyOcean <dljhazelh@gmail.com>
2021-09-30 14:20:20 -0400

    Worked on assignment three

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -2,10 +2,13 @@ import unittest
 import assignment_three
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class MyTestCase(assignment_three.TestCase):
+    def test_assignment_three(self):
+        self.assertEqual(25, assignment_three.rectangle_area(5, 5))
+        self.assertEqual(42, assignment_three.rectangle_area(6, 7))
+        #self.assertEqual(150, assignment_three.calculate_surface_area(5, 5, 5))
+        # add assertion here
 
 
 if __name__ == '__main__':
-    unittest.main()
+    assignment_three.main()
--- assignment_three.py
+++ assignment_three.py
@@ -1,18 +1,56 @@
 print("This program calculates the surface area of a rectangular prism")
-def get_width(x):
+def get_width():
+    """
+    Asks for a length then returns the answer as the function
+    :return:
+    """
     x = input("What is the width")
-    return x
-def get_height(y):
+    return float(x)
+def get_height():
+    """
+    Asks for a length then returns the answer as the function
+    :return:
+    """
     y = input("What is the height")
-    return y
-def get_length(z):
+    return float(y)
+def get_length():
+    """
+    Asks for a length then returns the answer as the function
+    :return:
+    """
     z = input("What is the length")
-    return z
+    return float(z)
+def rectangle_area(x, y):
+    side = x * y
+    return side
+def calculate_surface_area(x, y , z):
+    """
+    calculates all the surface areas then adds them. Spits out the answer as calculate_surface_area
+    :param x:
+    :param y:
+    :param z:
+    :return:
+    """
+    side1and2 = rectangle_area(x, y)* 2
+    side3and4 = rectangle_area(x, z) * 2
+    side5and6 = rectangle_area(y, z) * 2
+    total = side1and2 + side3and4 + side5and6
+    return total
+
+def reply(x, y, z, total):
+    print("Surface area of a rectangular prism with a width of", x, ",height of", y, ", and length of", z, "is", total)
 
 def main():
-    get_height(y)
-    get_width(x)
-    get_length(z)
+    """
+calls all of the functions then replies to the person
+also assigns functions a value
+    :return:
+    """
+    x = get_width()
+    y = get_height()
+    z = get_length()
+    total = calculate_surface_area(x, y ,z)
+    reply(x, y, z, total)
 
-
-main()
+if __name__ == '__main__':
+    main()
