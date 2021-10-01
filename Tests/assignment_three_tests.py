f2991f972f7a7eab594cb827e300420f12d51ba9
AFishyOcean <dljhazelh@gmail.com>
2021-10-01 12:12:04 -0400

    Worked on assignment three

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -7,7 +7,6 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(25, assignment_three.rectangle_area(5, 5))
         self.assertEqual(42, assignment_three.rectangle_area(6, 7))
         self.assertEqual(150, assignment_three.calculate_surface_area(5, 5, 5))
-        #self.assertEqual(150, assignment_three.calculate_surface_area(5, 5, 5))
         # add assertion here
 
 if __name__ == '__main__':
